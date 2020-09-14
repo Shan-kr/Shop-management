9d4fa49e62493f5552a1ceed177a9486b6d4a9ef
shantanu kumar <68410510+Shan-kr@users.noreply.github.com>
2020-09-14 22:42:40 +0530

    Update deletedata.php

--- php/deletedata.php
+++ php/deletedata.php
@@ -3,11 +3,10 @@ $table=$_POST["table"];
 $id=$_POST["id"];
 session_start();
 $_SESSION["refresh"]="no";
-$host="sql207.epizy.com";
-$dbusername="epiz_26741312";
-$dbpassword="3cxi9a9QPeT";
-$dbname="epiz_26741312_shopmanagement";
-
+    $host=" ";
+        $dbusername=" ";
+        $dbpassword=" ";
+        $dbname=" ";
 $conn = mysqli_connect("$host","$dbusername","$dbpassword","$dbname");
 
 if(!$conn)

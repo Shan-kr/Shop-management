5cb2101b5a7f6d3ba69a340d6c098d48b8a9ff00
shantanu kumar <68410510+Shan-kr@users.noreply.github.com>
2020-09-14 22:45:52 +0530

    Update updatedatap.php

--- php/updatedatap.php
+++ php/updatedatap.php
@@ -11,10 +11,11 @@ session_start();
 
 session_start();
 $_SESSION["refresh"]="no";
-$host="sql207.epizy.com";
-$dbusername="epiz_26741312";
-$dbpassword="3cxi9a9QPeT";
-$dbname="epiz_26741312_shopmanagement";
+
+    $host=" ";
+        $dbusername=" ";
+        $dbpassword=" ";
+        $dbname=" ";
 
 $conn = mysqli_connect("$host","$dbusername","$dbpassword","$dbname");
 
